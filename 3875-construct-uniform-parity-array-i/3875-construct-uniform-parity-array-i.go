0152d56f24657dbceb3ab22e360c8dbb1d4ff12e
Nitin Chandani <nitin1chandani@gmail.com>
2026-09-03 01:03:02 +0530

    Time: 0 ms (100%), Space: 4.8 MB (33.33%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.go
@@ -0,0 +1,29 @@
+func uniformArray(nums1 []int) bool {
+    // [1,2,3,4,5,6,7,8,9,10,11]
+    // 6 odd 5 even
+    // odd - odd is even
+    // even - even is even
+    // odd - even is odd
+
+    // oddNum := 0
+    // evenNum := 0
+
+    // oddEvenBothExist := false
+
+    // for _, v := range nums1{
+    //     if v % 2 == 0{
+    //         evenNum = v
+    //     }else{
+    //         oddNum = v
+    //     }
+
+    //     if oddNum!=0 && evenNum!=0{
+    //         oddEvenBothExist = true
+    //         break
+    //     }
+    // }
+
+    return true
+    
+
+}
